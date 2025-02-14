8fe3c399dd631b043e2de8ef7524868175a19b75
KennethTaipe06 <kataipeg@uce.edu.ec>
2025-02-14 17:11:49 -0500

    Configurar conexión a base de datos y agregar Dockerfile para despliegue

--- INVENTARIO/config.py
+++ INVENTARIO/config.py
@@ -1,12 +1,11 @@
-
 #conexion a base de datos para desarrollo
 
-#DB_CONFIG = {
-#    'host': 'localhost',
-#    'user': 'root',
-#    'password': 'Brytib#1906',
-#    'database': 'ruta_del_pan',
-#    }
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'Brytib#1906',
+    'database': 'ruta_del_pan',
+}
 
 
 #conexion a base de datos para docker - local
@@ -19,10 +18,10 @@
 #    }
 
 #conexion a base de datos para docker
-DB_CONFIG = {
-    'host': 'mysql',
-    'user': 'root',
-    'password': 'Brytib+1906',
-    'database': 'Ruta_del_Pan',
-    'port':3307
-    }
+#DB_CONFIG = {
+#    'host': 'mysql',
+#    'user': 'root',
+#    'password': 'Brytib+1906',
+#    'database': 'Ruta_del_Pan',
+#    'port':3307
+#    }
--- INVENTARIO/main.py
+++ INVENTARIO/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from routes.inventarios import inventario_bp
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 app.register_blueprint(inventario_bp)
 
 @app.route('/')
@@ -10,4 +10,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
